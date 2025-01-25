952ab24a1d079c9d62ec1298a2efdbf3850eb013
Kuwarpreet Singh <kuwarpreets@gmail.com>
2025-01-25 13:12:15 -0500

    changed vercel backend url;

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5003/api/v1";
+axios.defaults.baseURL = "chatbot-ashy-two.vercel.app";
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
